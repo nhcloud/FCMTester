2525efccfab1e6de18f3be1930ba417122587542
Udaiappa Ramachandran <udaiappa@gmail.com>
2024-02-12 23:20:01 -0500

    label typos

--- FcmTester/Models/PushNotificationModel.cs
+++ FcmTester/Models/PushNotificationModel.cs
@@ -4,26 +4,26 @@ using System.Net;
 namespace FcmTester.Models;
 public class PushNotificationModel
 {
-    [Display(Name = "Google FCM Token from JSON Key file [Required]")]
+    [Display(Name = "Google FCM Token from JSON Key file. [Required]")]
     [Required(ErrorMessage = "Google FCM Token from JSON Key file is required.")]
     public string FcmToken { get; set; }
 
-    [Display(Name = "Targetted device token, comma separate for multiple tokens [Required]")]
-    [Required(ErrorMessage = "Targetted device token is required.")]
+    [Display(Name = "Targeted device token. [Required]")]
+    [Required(ErrorMessage = "Targeted device token is required.")]
     public string DeviceToken { get; set; }
 
-    [Display(Name = "Notification Title [Required]")]
+    [Display(Name = "Notification title. [Required]")]
     [Required(ErrorMessage = "Title is required.")]
     public string Title { get; set; }
 
-    [Display(Name = "Notification Body [Required]")]
+    [Display(Name = "Notification body. [Required]")]
     [Required(ErrorMessage = "Body is required.")]
     public string Body { get; set; }
 
-    [Display(Name = "Notification Image Url [optional]")]
+    [Display(Name = "Notification image url. [optional]")]
     public string? ImageUrl { get; set; }
 
-    [Display(Name = "Notification click action [optional]")]
+    [Display(Name = "Notification click action. [optional]")]
     public string? ClickAction { get; set; }
 
     public ResultModel? Result { get; set; }
